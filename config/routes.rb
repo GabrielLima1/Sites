13d6dc0815a02aa16a0f470a9dc50340970fa3e3
Gabriel Lima <gabriellimatrajano@hotmail.com>
2018-04-13 13:44:07 -0300

    created all routes in project

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,27 @@ Rails.application.routes.draw do
   resources :accounts
   resources :compans
   resources :accessps
+
+  resources :app_produtcs
+  resources :apps
+  resources :archives
+  resources :be_clients
+  resources :candidate_works
+  resources :cat_ctrl_qualits
+  resources :compans
+  # resources :contact_us
+  resources :cores
+  resources :ctrl_qualits
+  resources :end_factors
+  resources :fac_contacts
+  resources :menus
+  resources :products
+  resources :shipping_emails
+  resources :socials
+  resources :sub_cat_ctrl_qualits
+  resources :sub_menus
+  resources :type_archives
+  resources :works
   # Para Adcionar um novo recurso use 'resources :nome_recurso_no_plural' sem aspas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
