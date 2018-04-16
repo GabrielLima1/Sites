38ba7f058260d3c841b8e1fab098fcacced674b4
Gabriel Lima <gabriellimatrajano@hotmail.com>
2018-04-16 09:26:08 -0300

    atualizando view application

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
   resources :sub_menus
   resources :type_archives
   resources :works
+  
   # Para Adcionar um novo recurso use 'resources :nome_recurso_no_plural' sem aspas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
